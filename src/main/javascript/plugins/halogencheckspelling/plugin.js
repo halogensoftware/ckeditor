826ace84ecc692d4649b0b07694ab1ae939a40d7
Louis Amstutz <lamstutz@halogensoftware.com>
2013-05-08 15:49:42 -0400

    EIX-20914: Created CKEditor 4.1 project

---
+++ src/main/javascript/plugins/halogencheckspelling/plugin.js
@@ -0,0 +1,52 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.add('halogencheckspelling', {
+    init: function (a) {
+        var b = 'halogencheckspellingcmd';
+        a.ui.addButton(b, {
+            label: a.lang.wsc.toolbar,
+            icon: this.path + 'spell-check.gif',
+            click: function (c) {
+                var d = false,
+                    e = CKEDITOR.basePath,
+                    f = CKEDITOR.basePath.replace('/ckeditor', ''),
+                    g = f + 'util/spellChecker_cfg_function.jsp';
+                CKEDITOR.scriptLoader.load(g, function (m) {
+                    if (m) d = true;
+                }, this, false);
+                var h = f + 'css/spellChecker.css',
+                    i = document.getElementById(h);
+                if (i == null) {
+                    var j = new CKEDITOR.dom.element('link');
+                    j.setAttributes({
+                        rel: 'stylesheet',
+                        type: 'text/css',
+                        href: h,
+                        id: h
+                    });
+                    j.appendTo(CKEDITOR.document.getHead());
+                }
+                var k = null;
+                if ($('languages')) k = HALOGEN_TO_SPELL_CHECKER_LANGUAGE_MAP[$('languages').options[$('languages').selectedIndex].value];
+                else k = CK_EDITOR_TO_SPELL_CHECKER_LANGUAGE_MAP[a.config.language]; if (!k) {
+                    alert('SpellChecker: Unsupported language!');
+                    return;
+                }
+                var h = CKEDITOR.basePath.replace('/common/ckeditor/', ''),
+                    l = function () {
+                        if (d) {
+                            var m = getSpellCheckerConfig();
+                            m.url = h;
+                            m.language = k;
+                            m.elements = [a.name];
+                            var n = new HalogenSpellChecker(m);
+                            n.show();
+                        } else setTimeout(l, 100);
+                    };
+                l();
+            }
+        });
+    }
+});
---
+++ src/main/javascript/plugins/halogenemailtemplatemergefields/plugin.js
@@ -0,0 +1,75 @@
+﻿/*
+Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.add('halogenemailtemplatemergefields', {
+    requires: ['richcombo'],
+    init: function (a) {
+        var b = a.config,
+            c = a.lang.format,
+            d = '',
+            e = 120,
+            f = 120;
+        a.ui.addRichCombo('halogenemailtemplatemergefields', {
+            label: CKEDITOR.lang.insertTags ? CKEDITOR.lang.insertTags : 'Insert tags',
+            title: CKEDITOR.lang.insertTags ? CKEDITOR.lang.insertTags : 'Insert tags',
+            voiceLabel: CKEDITOR.lang.insertTags ? CKEDITOR.lang.insertTags : 'Insert tags',
+            className: 'cke_format',
+            multiSelect: false,
+            panel: {
+                css: [b.contentsCss, CKEDITOR.getUrl(CKEDITOR.skin.path() + 'editor.css')],
+                voiceLabel: c.panelVoiceLabel
+            },
+            init: function () {
+                var i = this;
+                var g = a.config;
+                if (g && g.tokenselector && g.tokenselector.init) g.tokenselector.init.call(i, i, a);
+                else {
+                    i.startGroup('Tags');                    
+                    for (var h = 0; h < g.totalTags; h++) i.add(g.tags[h][0], unescape(g.tags[h][1]), unescape(g.tags[h][2]));
+                }
+            },
+            onClick: function (g, h) {
+                a.focus();
+                a.fire('saveSnapshot');
+                a.insertHtml(unescape(g));
+                a.fire('saveSnapshot');
+                CKEDITOR.tools.setTimeout(function () {
+                    this.unmarkAll();
+                    this.setValue('');
+                }, 0, this);
+            },
+            onOpen: function () {
+                var q = this;
+                if (q._ && q._.panel && q._.panel.element && q._.panel.element.$) {
+                    var g = q._.panel.element.$,
+                        h = g.getElementsByTagName('IFRAME');
+                    if (h) {
+                        h = h[0];
+                        if (d.length == 0) {
+                            var i = h.contentDocument || h.contentWindow.document;
+                            for (var j in q._.list._.items) {
+                                var k = i.getElementById(q._.list._.items[j]),
+                                    l = k.innerText || k.textContent;
+                                if (d.length < l.length) d = l + 20;
+                            }
+                            var m = h.parentNode,
+                                n = document.createElement('div'),
+                                o = {
+                                    position: 'absolute',
+                                    visibility: 'hidden',
+                                    overflow: 'scroll'
+                                };
+                            for (var p in o) n.style[p] = o[p];
+                            n.appendChild(document.createTextNode(d));
+                            m.appendChild(n);
+                            e = n.offsetWidth + Math.max(0, n.offsetWidth - n.clientWidth);
+                            m.removeChild(n);
+                        }
+                        h.parentNode.style.width = Math.max(f, e) + 'px';
+                    }
+                }
+            }
+        });
+    }
+});
